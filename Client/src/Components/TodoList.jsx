115387eaa780d96d92287553fecb3c1fea5e697e
thusharkeee <4su21ee007@sdmit.in>
2025-01-20 11:34:19 +0530

    Fourth

--- Client/src/Components/TodoList.jsx
+++ Client/src/Components/TodoList.jsx
@@ -13,7 +13,7 @@ const [currentTodo, setCurrentTodo] = useState({_id: null, message: ''});
 
 const getAllTodos = async () => {
     try {
-        const response = await axios.get('http://localhost:5000/todolist/getall');
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/todolist/getall`);
         setTodos(response.data.data);
         } catch (error) {
         console.error(error);
@@ -37,7 +37,7 @@ useEffect(() => {
 
 const handleDelete = async (id) => {
     try {
-        const result = await axios.delete(`http://localhost:5000/todolist/deleteToDo/${id}`);
+        const result = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/todolist/deleteToDo/${id}`);
         if (result.data.success === 'deleted') {
             toast.success('Todo deleted successfully!');
             getAllTodos();
@@ -65,7 +65,7 @@ const handleUpdate = async () => {
     }
 
     try {
-        const result = await axios.put(`http://localhost:5000/todolist/updateToDo/${currentTodo._id}`, {
+        const result = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/todolist/updateToDo/${currentTodo._id}`, {
            message: currentTodo.message 
         });
         if (result.data.success === 'updated') {
